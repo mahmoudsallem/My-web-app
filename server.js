8bf70fa1e2cc096e4a4312b6f540e96e0b97862c
mahmoudsallem <salem7ouda@gmail.com>
2025-11-02 17:21:53 +0200

    add all files

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.post('/submit', (req, res) => {
+    const userData = req.body;
+    fs.readFile('users.json', 'utf8', (err, data) => {
+        if (err && err.code !== 'ENOENT') {
+            console.error(err);
+            return res.status(500).send('Error reading user data.');
+        }
+        const users = data ? JSON.parse(data) : [];
+        users.push(userData);
+        fs.writeFile('users.json', JSON.stringify(users, null, 2), (err) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).send('Error saving user data.');
+            }
+            res.status(200).send('User data saved successfully.');
+        });
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
